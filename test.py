3de2c59a5f4dafad9584f9afde8c4823cf6b133e
OEM Configuration (temporary user) <guyarieli17@gmail.com>
2021-09-26 18:35:25 +0300

    defualt

---
+++ test.py
@@ -0,0 +1,7 @@
+import cityflow
+eng = cityflow.Engine('./config.json')
+for i in range(1000):
+    if i == 50:
+        eng.set_tl_phase("intersection_0_0", 1)
+    eng.next_step()
+print(eng.get_current_time())
