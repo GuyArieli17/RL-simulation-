566d87c42bffcd204951d6e782102653555d4777
OEM Configuration (temporary user) <guyarieli17@gmail.com>
2021-09-30 15:39:42 +0300

    add auto json editor

--- src/test.py
+++ src/test.py
@@ -1,8 +1,20 @@
 import cityflow
-eng = cityflow.Engine('simulation/json/config.json')
-for i in range(1000):
-    if i == 50:
-        eng.set_tl_phase("intersection_0_0", 1)
-    eng.next_step()
-print(eng.get_current_time())
+import os
+from dotenv import load_dotenv
+from auto_option_builder import OptionBuilder
+from json_handler import JsonHandler
+load_dotenv()
+
+#eng = cityflow.Engine(os.getenv('CONFIG_JSON_FILE'))
+
+#print(OptionBuilder.generate(30,4))
+
+JsonHandler.append_to_intersection('./simulation/json/roadnet',"intersection_0_0",[(2,[0,1])])
+print(JsonHandler.read('./simulation/json/roadnet'))
+
+
+
+#for i in range(1000):
+#    eng.next_step()
+#print(eng.get_current_time())
 
