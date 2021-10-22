bf9d435d0e152dbad0884d9043973b2ccc9485e4
Guy Arieli <guyarieli17@gmail.com>
2021-10-22 11:14:30 +0300

    message

--- src/test.py
+++ src/test.py
@@ -3,18 +3,30 @@ import os
 from dotenv import load_dotenv
 from auto_option_builder import OptionBuilder
 from json_handler import JsonHandler
+
 load_dotenv()
 
-#eng = cityflow.Engine(os.getenv('CONFIG_JSON_FILE'))
+eng = cityflow.Engine(os.getenv("CONFIG_JSON_FILE"))
+for i in range(5000):
+    if i == 200:
+        eng.set_tl_phase("intersection_1_1", 0)
 
-#print(OptionBuilder.generate(30,4))
+    eng.next_step()
 
-JsonHandler.append_to_intersection('./simulation/json/roadnet',"intersection_0_0",[(2,[0,1])])
-print(JsonHandler.read('./simulation/json/roadnet'))
 
+
+
+
+# print(OptionBuilder.generate(30,4))
+
+#JsonHandler.append_to_intersection("./simulation/json/roadnet", "intersection_0_0", [(2, [0, 1])])
+# print(JsonHandler.read('./simulation/json/roadnet'))
 
 
 #for i in range(1000):
-#    eng.next_step()
-#print(eng.get_current_time())
+    # if i == 500:
+    # eng.set_tl_phase("intersection_0_0", 1)
+    #eng.next_step()
+# print(eng.get_current_time())
+
 
